c5c1a8b54928a5bb4dfb1407361a0a097d668828
Hamed <hamed.mamdoohi@gmail.com>
2020-12-07 20:46:54 -0500

    add migration for each entity

---
+++ agent/migration/migrations/20201207200736_create_agent_table.js
@@ -0,0 +1,18 @@
+exports.up = async function (knex) {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS CITEXT')
+  return knex.schema.createTable('agents', (t) => {
+    t.uuid('id').defaultTo(knex.raw('uuid_generate_v4()'))
+    t.string('user_sub', 60).notNull()
+    t.string('name', 50).notNull()
+    t.specificType('email', 'CITEXT').unique().notNull()
+    t.boolean('is_active').defaultTo(false)
+    t.boolean('has_confirmed_email').defaultTo(false)
+    t.uuid('brokerage_id')
+    t.uuid('plan_id')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('agents')
+}
---
+++ property/migration/migrations/20201207204228_create_property_table.js
@@ -0,0 +1,11 @@
+exports.up = async function (knex) {
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  return knex.schema.createTable('properties', (t) => {
+    t.uuid('id').defaultTo(knex.raw('uuid_generate_v4()'))
+    t.string('name', 50).notNull()
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('properties')
+}
